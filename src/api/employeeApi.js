15c8bc1c3dc03976c57c4ebae999575ef93bdb54
sean-egloff-capgemini <sean.egloff@capgemini.com>
2021-09-03 09:54:41 -0400

    detailed comments

--- src/api/employeeApi.js
+++ src/api/employeeApi.js
@@ -2,6 +2,19 @@ import axios from "axios";
 
 const baseUrl = process.env.REACT_APP_API_URL + "employees/";
 
+/*
+ * employees/        -> needs error code if no database exists
+ *   - GET    -> get all employees
+ *   - POST   -> add new employee
+ * employees/:id     -> needs error code if no database exists
+ *   - GET    -> get employee by id
+ *   - PUT    -> update employee with given id
+ *   - DELETE -> delete employee with given id
+ * files/
+ *   - GET    -> get excel version of database
+ *   - POST   -> create database from excel file
+ */
+
 export function getEmployees(id) {
     return new Promise((resolve, reject) => {
         axios
